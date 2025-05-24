258294a9663f922c08a75dca70506fe1855550ed
350349619@gapps.yrdsb.ca <howhiisahop@gmail.com>
2025-05-24 19:02:04 -0400

    Added Collision Test and Counter

--- pac_test.py
+++ pac_test.py
@@ -9,19 +9,26 @@ class PacWindow(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Collision Sprite Test")
         self.sprite_list = arcade.SpriteList()
+
         self.pacman = arcade.Sprite("Pacman_OG-removebg-preview.png", scale = 0.5)
         self.pacman.center_x = SCREEN_WIDTH // 2
         self.pacman.center_y = SCREEN_HEIGHT // 2
         self.sprite_list.append(self.pacman)
 
         self.ball = arcade.Sprite("ball.png", scale = 0.15)
+        self.reset_ball()
+        self.sprite_list.append(self.ball)
+
+        self.score =0
+
+    def reset_ball(self):
         self.ball.center_x = random.randint(0, SCREEN_WIDTH)
         self.ball.center_y = random.randint(0, SCREEN_HEIGHT)
-        self.sprite_list.append(self.ball)
 
     def on_draw(self):
         self.clear()
         self.sprite_list.draw()
+        arcade.draw_text(f"{self.score}", 10, SCREEN_HEIGHT - 30, arcade.color.WHITE, 20)
 
     def on_key_press(self, key, modifiers):
         if self.pacman:
@@ -33,6 +40,11 @@ class PacWindow(arcade.Window):
                 self.pacman.center_x -= MOVEMENT_SPEED
             elif key == arcade.key.RIGHT:
                 self.pacman.center_x += MOVEMENT_SPEED
+    
+    def on_update(self, delta_time):
+        if arcade.check_for_collision(self.pacman, self.ball):
+            self.score += 1
+            self.reset_ball()
 
 def main():
     window = PacWindow()
