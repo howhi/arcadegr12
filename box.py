255d24502ded58f6e152c46d2cbeb3dd27d937a3
350349619@gapps.yrdsb.ca <howhiisahop@gmail.com>
2025-05-27 11:14:37 -0400

    Added comments to box.py

--- box.py
+++ box.py
@@ -1,17 +1,22 @@
 import arcade
 
+# Screen & box constants
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 400
 BOX_SIZE = 50
 MOVEMENT_SPEED = 10
 
+#Main Window class
 class BoxWindow(arcade.Window):
     def __init__(self):
+        #Initialize the window with size and title
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Move the Box")
+        #Set initial box position at centre of screen
         self.box_x = SCREEN_WIDTH // 2
         self.box_y = SCREEN_HEIGHT // 2
-    
+
     def on_draw(self):
+        #Clear screen and draw box
         self.clear()
         arcade.draw_lrbt_rectangle_filled(
             left = self.box_x - BOX_SIZE / 2,
@@ -22,6 +27,7 @@ class BoxWindow(arcade.Window):
         )
 
     def on_key_press(self, key, modifiers):
+        #Move box according to arrow keys pressed
         if key == arcade.key.UP:
             self.box_y += MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
@@ -30,11 +36,12 @@ class BoxWindow(arcade.Window):
             self.box_x -= MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
             self.box_x += MOVEMENT_SPEED
-        
+
+#Launch window and run main loop
 def main():
     window = BoxWindow()
     arcade.run()
 
-
+#Run program
 if __name__ == "__main__":
     main()
